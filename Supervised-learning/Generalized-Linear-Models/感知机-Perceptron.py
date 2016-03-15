bcc9954bc979256797f84e45b9f5352a1c422032
sxiong <sxiong@hust.edu.cn>
2016-03-15 17:28:47 +0800

    add ganzhiji

--- Supervised-learning/Generalized-Linear-Models/感知机-Perceptron.py
+++ Supervised-learning/Generalized-Linear-Models/感知机-Perceptron.py
@@ -1,1 +1,15 @@
-dd
+import numpy as np
+from sklearn.linear_model import SGDClassifier, Perceptron
+
+'''
+特点：
+	二分类
+	特征空间线性可分
+'''
+X = np.array([(3,3),(4,3),[1,1]])
+y = np.array(['b','b','a'])
+module = Perceptron(n_iter=20,verbose=1)
+print module.get_params()
+module.fit(X,y)
+print module.predict(np.array([(0,0),(2,10),(4,5),(1,1)]))
+print module.get_params()
